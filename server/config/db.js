fc440b98fbc19e22e201dd96dba2f2bfd38a3a1c
rickynguyen1707 <55204776+rickynguyen1707@users.noreply.github.com>
2020-10-25 21:18:22 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 // COMP229-F2020-Assignment2, Nguyen Khang Nguyen, 301098234, 10/25/2020
 module.exports = 
 {
-    "URI": "mongodb://localhost/contact_list"
+    //"URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://khangnguyen:JIq4n2VOAlQjZTUn@mongodbserver.6ermn.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
